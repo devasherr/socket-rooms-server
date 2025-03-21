396cbea122afdb0e51a26274afa2529b7370d716
Asher Samuel <ashersam116@gmail.com>
2025-03-21 11:51:28 +0300

    login and logout implemented

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/devasherr/socket-rooms
 go 1.24.1
 
 require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt/v4 v4.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.36.0
 )
@@ -14,7 +16,6 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
--- internal/user/user_service.go
+++ internal/user/user_service.go
@@ -6,6 +6,11 @@ import (
 	"time"
 
 	"github.com/devasherr/socket-rooms/util"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const (
+	secretKey = "very_secret_stuff"
 )
 
 type service struct {
@@ -47,4 +52,40 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	}
 
 	return res, nil
+}
+
+type MyJwtClaims struct {
+	ID       string `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	jwt.RegisteredClaims
+}
+
+func (s *service) Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	if err := util.CheckPassword(req.Password, u.Password); err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyJwtClaims{
+		ID:       strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return &LoginUserRes{}, nil
+	}
+
+	return &LoginUserRes{accessToken: ss, Username: u.Username, ID: strconv.Itoa(int(u.ID))}, nil
 }
